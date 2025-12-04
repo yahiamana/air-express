fdcd91c9586dea3a7743bfe0cfe03e43b25b3fe5
Yahiamana <yahiamana4@gmail.com>
2025-12-04 10:04:40 +0100

    Update: New features added yahia

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import type { MetadataRoute } from 'next'
  
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.NEXT_PUBLIC_APP_URL || 'https://airexpress.com'
+  const baseUrl = 'https://airexpress.com'
   
   return {
     rules: [
